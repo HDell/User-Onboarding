2d690f01f10fd3b7a7776daeb8683af03c9c9f1b
Handell Desulme <handelldesulme@Handells-MacBook-Pro.local>
2019-12-20 15:35:49 -0500

    completes 3/4 of mvp

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './components/Form';
+import FormList from './components/FormList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+      <FormList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/components/FormList.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import Form from './Form';
+
+function FormList() {
+    return (
+      <div>
+          Test
+      </div>
+    );
+}
+
+export default FormList;
